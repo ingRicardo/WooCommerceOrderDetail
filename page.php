fb727c0f5690ce5238b0233820ff83d8e5b372ce
Ricardo Alfredo Macias Olvera <ra_mo116@hotmail.com>
2019-04-23 11:34:09 -0700

    Add files via upload

--- page.php
+++ page.php
@@ -55,9 +55,6 @@ get_header(); ?>
 			//http.open("GET", url+"?"+params, true);
 			//Send the proper header information along with the request
 			http.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-			http.setRequestHeader("Content-length", params.length);
-			http.setRequestHeader("Connection", "close");
-
 			http.onreadystatechange = function() {//Call a function when the state changes.
 				if(http.readyState == 4 && http.status == 200) {
 					alert(http.responseText);
